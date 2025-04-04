30b814e09919d6005dd00514f3044c1be46de1cd
Ben Mcleod <ben-mcleod@live.co.uk>
2025-04-04 21:23:39 +0100

    Add car park and parking space models

--- app/Models/CarPark.php
+++ app/Models/CarPark.php
@@ -1,10 +1,49 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CarPark extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'total_spaces'];
+
+    public function parkingSpaces(): HasMany
+    {
+        return $this->hasMany(ParkingSpace::class);
+    }
+
+    public function bookings(): HasMany
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    public function totalSpaces(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->parkingSpaces()->count()
+        );
+    }
+
+    public function checkAvailability(Carbon $from, Carbon $to, ?int $excludeId = null): int
+    {
+        $overlappingBookingsCount = $this->bookings()
+            ->overlappingWithDates($from, $to)
+            ->when($excludeId, function ($query) use ($excludeId) {
+                $query->where('id', '!=', $excludeId);
+            })
+            ->count();
+
+        $availableSpaces = $this->total_spaces - $overlappingBookingsCount;
+
+        return max($availableSpaces, 0);
+    }
 }
--- app/Models/ParkingSpace.php
+++ app/Models/ParkingSpace.php
@@ -1,10 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ParkingSpace extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['car_park_id'];
+
+    public function carPark(): BelongsTo
+    {
+        return $this->belongsTo(CarPark::class);
+    }
 }
