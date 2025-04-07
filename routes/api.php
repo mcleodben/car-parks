ba95339a681846a7b463b089b60e4fbd44cdf054
Ben Mcleod <ben-mcleod@live.co.uk>
2025-04-07 19:52:45 +0100

    Add all routes

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+declare(strict_types=1);
+
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CarParkController;
 
 Route::controller(AuthController::class)->group(function() {
     Route::post('register', 'register');
     Route::post('login', 'login');
 });
 
+Route::controller(CarParkController::class)->group(function() {
+    Route::get('/carpark/{carPark}', 'show');
+    Route::get('/carpark', 'index');
+});
+
 Route::middleware('auth:sanctum')->group(function() {
+    Route::resource('bookings', BookingController::class);
     Route::post('logout', [AuthController::class, 'logout']);
 });
